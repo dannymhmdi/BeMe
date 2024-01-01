7d3c5223229c995f15d7d11a3328688e2bd4e282
mohammadreza <mmdrza77@gmail.com>
2024-01-01 18:51:04 +0330

    develop landing

--- app/page.js
+++ app/page.js
@@ -5,13 +5,13 @@ export default function Home() {
   return (
     <>
       <LandingLayout>
-        <BasePage fluid={"false"}>
+        <BasePage fixed={true}>
           <div className="flex flex-col justify-self-center">
-            <div className="text-6xl font-bold flex justify-center">
+            <div className="text-6xl font-bold flex">
               <span className="">BeMe. </span> <span> Be anyone You Want</span>
             </div>
 
-            <div className="flex justify-center mt-10">
+            <div className="flex mt-10">
               <div className="p-2" style={{ border: "1px solid black" }}>
                 <div>Where</div>
                 <TextField placeholder="AnyWhere" variant="standard" />
@@ -26,12 +26,14 @@ export default function Home() {
               </div>
             </div>
 
-            <div className="flex justify-center flex-col">
-              <p className="font-bold text-5xl">
-                Experience a different life.
-              </p>
-              <p className="font-bold text-5xl">Get inspired.</p>
-              <p className="font-bold text-4xl">
+            <div className="flex flex-col">
+              <div className="flex flex-col py-10">
+                <p className="font-bold text-5xl">
+                  Experience a different life.
+                </p>
+                <p className="font-bold text-5xl">Get inspired.</p>
+              </div>
+              <p className="font-bold text-4xl" style={{width: '940px'}}>
                 Try any profession for a few hours with our vetted marketplace
                 of industry experts.
               </p>
